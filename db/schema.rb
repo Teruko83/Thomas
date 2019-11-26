a646dc97083a1d955d791d61a1e6d0b3bd85a3b4
aurelie mathys <mathyau@gmail.com>
2019-11-26 16:47:37 -0500

    better seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_210126) do
+ActiveRecord::Schema.define(version: 2019_11_26_205936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_210126) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_babies_on_user_id"
-  end
-
-  create_table "bottlefeedings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "breastfeedings", force: :cascade do |t|
