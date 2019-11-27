e7361fc4797b6c3f72335b416f5697ed2dd9e0f1
Adam Bedard <adam.bedard@icloud.com>
2019-11-27 17:29:53 -0500

    DB bug with bottle feeding resolved

--- app/controllers/bottle_feedings_controller.rb
+++ app/controllers/bottle_feedings_controller.rb
@@ -34,14 +34,13 @@ class BottleFeedingsController < ApplicationController
 
   def create
     @baby = Baby.find(params[:baby_id])
-    bottle_feeding = Bottlefeeding.new
-    bottle_feeding.baby = @baby
-    bottle_feeding.quantity = params[:quantity_feeded]
-    if bottle_feeding.save
+    @bottle_feeding = Bottlefeeding.new
+    @bottle_feeding.baby = @baby
+    @bottle_feeding.quantity = params[:quantityFeeded]
+    if @bottle_feeding.save
       redirect_to user_path(current_user)
     else
       render "alert"
     end
   end
-
 end
