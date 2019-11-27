8e5b0464e29e791db7dfac2e069b787ea967e7e1
Adam Bedard <adam.bedard@icloud.com>
2019-11-27 16:22:22 -0500

    Bottle feeding all done

--- app/controllers/bottle_feedings_controller.rb
+++ app/controllers/bottle_feedings_controller.rb
@@ -3,9 +3,18 @@ class BottleFeedingsController < ApplicationController
   end
 
   def new
-    @bottle_feeding = Bottlefeeding.new
+    @baby = Baby.find(params[:baby_id])
   end
 
   def create
+    @baby = Baby.find(params[:baby_id])
+    bottle_feeding = Bottlefeeding.new
+    bottle_feeding.baby = @baby
+    bottle_feeding.quantity = params[:quantity_feeded]
+    if bottle_feeding.save
+      redirect_to user_path(current_user)
+    else
+      render "alert"
+    end
   end
 end
