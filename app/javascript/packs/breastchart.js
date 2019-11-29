1576d1693af375db0b30d75b0d6a060c123526e7
Athena Vassilopoulos <athena.vassilo@gmail.com>
2019-11-29 13:15:26 -0500

    re arranged controllers for breastfeedings and bottlefeedings to make sure graphs work

--- app/javascript/packs/breastchart.js
+++ app/javascript/packs/breastchart.js
@@ -8,31 +8,31 @@ Chart.defaults.global.defaultFontFamily = 'Lato';
 Chart.defaults.global.defaultFontSize = 18;
 Chart.defaults.global.defaultFontColor = '#777';
 
-let ConsumptionChart = new Chart(myChart, {
-  type:'line', // options: bar, horizontalBar, pie, line, doughnut, radar, polarArea
-  data:{
-    datasets:[{
-      label:'Last seven days',
-      data: chartTest,
-      backgroundColor:'#0F0D25',
-      borderWidth:1,
-      borderColor:'#777',
-      hoverBorderWidth:3,
-      hoverBorderColor:'#000'
-    }]
-  },
-  options: {
-    scales: {
-        xAxes: [{
-            type: 'time',
-            distribution: 'series',
-            time: {
-                unit: 'day'
-            }
-        }]
-    }
-}
-});
+// let ConsumptionChart = new Chart(myChart, {
+//   type:'line', // options: bar, horizontalBar, pie, line, doughnut, radar, polarArea
+//   data:{
+//     datasets:[{
+//       label:'Last seven days',
+//       data: chartTest,
+//       backgroundColor:'#0F0D25',
+//       borderWidth:1,
+//       borderColor:'#777',
+//       hoverBorderWidth:3,
+//       hoverBorderColor:'#000'
+//     }]
+//   },
+//   options: {
+//     scales: {
+//         xAxes: [{
+//             type: 'time',
+//             distribution: 'series',
+//             time: {
+//                 unit: 'day'
+//             }
+//         }]
+//     }
+// }
+// });
 document.getElementById("last7").addEventListener("click", getLastSevenConsumption);
 const getLastSevenConsumption = () => {
   // Parse data from rails view
@@ -40,14 +40,13 @@ const getLastSevenConsumption = () => {
   console.log('raw dataset', rawDataset)
   const finalData = [];
   rawDataset.forEach(record => {
-    const xValue = record.start;
+    const xValue = record.start_date;
     const yValue = record.duration_minutes;
     const feeding = { x: xValue, y: yValue };
     finalData.push(feeding);
   })
 
-
-  ConsumptionChart = new Chart(myChart, {
+  let ConsumptionChart = new Chart(myChart, {
       type:'line', // options: bar, horizontalBar, pie, line, doughnut, radar, polarArea
       data:{
         datasets:[{
