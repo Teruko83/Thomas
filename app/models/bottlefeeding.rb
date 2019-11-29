c120d30fcb6225e5e7f5c8956a7765192aa3957c
aurelie mathys <mathyau@gmail.com>
2019-11-28 19:13:55 -0500

    Solving bottlefeedings issues

--- app/models/bottlefeeding.rb
+++ app/models/bottlefeeding.rb
@@ -1,3 +1,9 @@
 class Bottlefeeding < ApplicationRecord
   belongs_to :baby
+
+  def self.show_feedings(id)
+    Bottlefeeding.where(baby_id: id).each do |bottle|
+      "<div>#{bottle.quantity}</div>"
+    end
+  end
 end
