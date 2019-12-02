fe7f7442a99685cbb1f36a5fbdcb7c364339c01b
aurelie mathys <mathyau@gmail.com>
2019-12-02 17:07:01 -0500

    diaper 2 pages

--- app/controllers/diapers_controller.rb
+++ app/controllers/diapers_controller.rb
@@ -1,11 +1,24 @@
 class DiapersController < ApplicationController
   def index
+    @baby = Baby.find(params[:baby_id])
+    @diapers = @baby.diapers
   end
 
   def new
+    @baby = Baby.find(params[:baby_id])
+    @diaper = Diaper.new
   end
 
   def create
+    @baby = Baby.find(params[:baby_id])
+    @diaper = Diaper.new
+    @diaper.start_date = DateTime.now
+    @diaper.baby = @baby
+      if @diaper.save
+      redirect_to baby_diapers_path(@baby)
+      else
+      render "alert"
+      end
   end
 
   def destroy
